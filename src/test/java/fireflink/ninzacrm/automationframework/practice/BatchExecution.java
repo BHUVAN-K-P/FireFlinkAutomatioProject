fd2457bdec8118e75102ecea3210eb147f6b454c
Bhuvan K P <Bhuvan K P@FFE-Bhuvan-KP>
2025-08-23 12:11:46 +0530

    batching xml

---
+++ src/test/java/fireflink/ninzacrm/automationframework/practice/BatchExecution.java
@@ -0,0 +1,11 @@
+package fireflink.ninzacrm.automationframework.practice;
+
+import org.testng.annotations.Test;
+
+public class BatchExecution {
+
+	@Test
+	public void Batching() {
+		System.out.println("Batch execution started......");
+	}
+}
